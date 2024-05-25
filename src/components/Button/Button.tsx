6ea8a01eca4c2695cc5c286e4c103e3f321fed0a
adiouma-dione <adiouma10@gmail.com>
2024-05-25 18:15:30 +0100

    restart the projet with JS

--- src/components/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button as NativeBaseButton, IButtonProps } from 'native-base';
-
-const Button: React.FC<IButtonProps> = (props) => {
-  return <NativeBaseButton {...props} />;
-};
-
-export default Button;
--- src/navigation/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AppNavigator } from './AppNavigator';
-export { default as HomeStackNavigator } from './HomeStackNavigator';
-export { default as SettingsStackNavigator } from './SettingsStackNavigator';
