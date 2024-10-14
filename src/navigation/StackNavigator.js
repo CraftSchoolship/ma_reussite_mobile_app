b51aa6184e1dcd8f92102168b035407d82967e61
Berthonge-Geek <berthonge.mibambou.tongo@craftschoolship.com>
2024-10-14 03:49:10 +0100

    [ENHANCEMENT] - removing empty spacesr

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -14,28 +14,21 @@ const Stack = createStackNavigator();
 export const StackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Splash">
-      {/* Splash Screen */}
       <Stack.Screen
         options={{ headerShown: false }}
         name="Splash"
         component={SplashScreen}
       />
-
-      {/* Policy Screen */}
       <Stack.Screen
         options={{ headerShown: false }}
         name="Policy"
         component={PolicyScreen}
       />
-
-      {/* Drawer Navigator (Main App) */}
       <Stack.Screen
         options={{ headerShown: false }}
         name="DrawerNavigator"
         component={DrawerNavigator}
       />
-
-      {/* Login Screen */}
       <Stack.Screen
         options={{ headerShown: true, header: () => <LoginScreenBanner /> }}
         name="Login"
